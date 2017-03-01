bd4c537126a35e75ca5ce723bd873e9410f6655e
Adam Winn <adamw005@yahoo.com>
2017-03-01 00:01:04 -0600

    aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
 	    s3_region: ENV["AWS_REGION"],
 	    s3_credentials: {
 	      s3_host_name: ENV["AWS_HOST_NAME"],
-	      bucket: ENV["AWS_BUCKET"],
+	      bucket: ENV["S3_BUCKET_NAME"],
 	      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
 	      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 	      }
