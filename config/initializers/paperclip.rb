f7a1e6c31179fe23da32738fc0fceefb4a834c96
Adam Winn <adamw005@yahoo.com>
2017-02-28 23:25:22 -0600

    aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,12 +3,14 @@
 # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 # Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
 
 
 # When only top Section:
 	# Read-only file system @ dir_s_mkdir - /projects
 # When only bottom Section:
-	#
+	# It doesn't use AWS, uses local storage
 # When both:
 	# Read-only file system @ dir_s_mkdir - /projects
+# When none:
+	# 
