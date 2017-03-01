204045d43a28b9a166005c1f9321d2db378401b8
Adam Winn <adamw005@yahoo.com>
2017-02-28 23:23:24 -0600

    aws

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,14 @@
 # Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-7.0.5-Q16'
 
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+
+
+# When only top Section:
+	# Read-only file system @ dir_s_mkdir - /projects
+# When only bottom Section:
+	#
+# When both:
+	# Read-only file system @ dir_s_mkdir - /projects
