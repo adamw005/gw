6c017a1ca26aa64ee99920ee96425745819323d4
Adam Winn <adamw005@yahoo.com>
2017-03-02 17:59:24 -0600

    recaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+	devise_for :users, :controllers => {:registrations => "registrations"}
   get 'pages/landing'
   get 'projects/show/:id', to: 'projects#show', as: 'projects'
   get 'projects/index'
