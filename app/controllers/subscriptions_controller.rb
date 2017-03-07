78622f1a96a1bfbc154d19c389f7e376fe0a925e
Adam Winn <adamw005@yahoo.com>
2017-03-06 19:04:59 -0600

    One subscription per project-rewards_tier-user

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,5 @@
 class SubscriptionsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
-	before_action :amount_matches_tier, only: [:new, :create]
 
   def show
 		# @subscription = Subscription.find(params[:id])
@@ -15,25 +14,21 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    @subscription = Subscription.new subscription_params
-    if @subscription.save
-      redirect_to projects_path(@subscription.project.id)
-    else
-			# If it doesn't save, it's probably because use can only subscribe to project-rewards_tier once [	validates_uniqueness_of :user_id, scope: :rewards_tier_id ] in subscription.rb
-      redirect_to projects_path(@subscription.project.id)
-    end
+		if subscription_params[:amount].to_i >= RewardsTier.find(subscription_params[:rewards_tier_id]).min_amount.to_i
+	    @subscription = Subscription.new subscription_params
+	    if @subscription.save
+	      redirect_to projects_path(@subscription.project.id)
+	    else
+				# If it doesn't save, it's probably because use can only subscribe to project-rewards_tier once [	validates_uniqueness_of :user_id, scope: :rewards_tier_id ] in subscription.rb
+	      redirect_to projects_path(@subscription.project.id)
+	    end
+		end
   end
 
 	private
 
 	def subscription_params
 		params.require(:subscription).permit(:type, :amount, :project_id, :user_id, :rewards_tier_id)
-	end
-
-	def amount_matches_tier
-		if subscription_params[:amount].to_i >= RewardsTier.find(subscription_params[:rewards_tier_id]).min_amount.to_i
-			raise 'Error: Wrong Rewards Tier'
-		end
 	end
 
 end
