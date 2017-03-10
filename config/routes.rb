be7c7a44ec78d4762740faae1cde64df992a1c3d
Adam Winn <adamw005@yahoo.com>
2017-03-10 16:05:03 -0600

    add card

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 	post 'comments/create'
 	post 'subscriptions/create'
 	post 'stripe_infos/create'
-	delete 'subscriptions/destroy/:id', to: 'subscriptions#destroy'
+	delete 'subscriptions/destroy/:id', to: 'subscriptions#destroy', as: 'subscriptions_destroy'
 	root to: 'pages#landing'
 
 	resources :charges
