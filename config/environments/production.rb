e6354ec75272ee0b0a5de69f3cc34744c4417d21
Adam Winn <adamw005@yahoo.com>
2017-04-11 13:18:13 -0500

    Add email functionality to Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
 	# Devise Mailer
 	ActionMailer::Base.delivery_method = :smtp
 	ActionMailer::Base.smtp_settings = {
-		:tls => true,
+		enable_starttls_auto: true,
 		:address => "smtp.gmail.com",
 		:port => "587",
 		:domain => "gmail.com",
