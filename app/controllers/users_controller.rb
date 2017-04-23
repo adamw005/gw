b671e381b8277e4f8576ce85f1b7d197fae490fd
Adam Winn <adamw005@yahoo.com>
2017-04-23 10:08:08 -0500

    Adding Stripe Withdrawal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
 
 	def user_params
-		params.require(:user).permit(:user_id)	# Will need to add `account` and `amount` to this
+		params.permit(:user_id)	# Will need to add `account` and `amount` to this
 	end
 
 
