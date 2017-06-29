e00f13c6920727e83535440c582f066259bc2bbf
Adam Winn <adam.winn@eimprovement.com>
2017-06-29 16:16:37 -0500

    adding rss

--- app/controllers/rss_s3s_controller.rb
+++ app/controllers/rss_s3s_controller.rb
@@ -1,11 +1,11 @@
 class RssS3sController < ApplicationController
 
 	def new
-    @rss_s3 = rss_s3.new
+    @rss_s3 = RssS3.new
   end
 
   def create
-    @rss_s3 = rss_s3.new rss_s3_params
+    @rss_s3 = RssS3.new rss_s3_params
     if @rss_s3.save
       redirect_to projects_path(@rss_s3.project_id, anchor: "RssS3s")
     else
