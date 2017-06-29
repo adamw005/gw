82db932b07e6613920e10303eb130f69b9f4035d
Adam Winn <adam.winn@eimprovement.com>
2017-06-29 17:06:24 -0500

    adding rss

--- app/controllers/rss_s3s_controller.rb
+++ app/controllers/rss_s3s_controller.rb
@@ -7,7 +7,7 @@ class RssS3sController < ApplicationController
   def create
     @rss_s3 = RssS3.new rss_s3_params
     if @rss_s3.save
-      redirect_to projects_path(@rss_s3.project_id, anchor: "RssS3s")
+      redirect_to projects_path(@rss_s3.project_id)
     else
       render :action => 'new'
     end
@@ -21,7 +21,7 @@ class RssS3sController < ApplicationController
 	private
 
 	def rss_s3_params
-		params.require(:rss_s3).permit(:file)
+		params.require(:rss_s3).permit(:file, :project_id)
 	end
 
 end
