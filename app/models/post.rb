4932eab4ddb04022607dd4f332804dadd17141c9
Adam Winn <adam.winn@eimprovement.com>
2017-06-30 18:09:16 -0500

    adding rss

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 	# obfuscate_id :spin => 72894092
   belongs_to :project
   has_one :rss_s3
-  accepts_nested_attributes_for :rss_s3
+  accepts_nested_attributes_for :rss_s3s
 end
